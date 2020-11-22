2ee88dc637e31f4e1059e77294d5406df880edb6
Nelson <tomneo2004@gmail.com>
2020-11-22 12:09:29 +0800

    api model type

--- src/utils/apiModelTypes.ts
+++ src/utils/apiModelTypes.ts
@@ -24,3 +24,105 @@ export interface ISearchMovieData{
     total_pages: number;
     results: IMovieData[];
 }
+
+export interface IGenreData {
+    id: number;
+    name: string;
+}
+
+export interface IProductionCompanyData {
+    name: string;
+    id: number;
+    logo_path: string | null;
+    origin_country: string;
+}
+
+export interface IProductionCountryData {
+    iso_3166_1: string;
+    name: string;
+}
+
+export interface ISpokenLanguageData {
+    iso_639_1: string;
+    name: string;
+}
+
+export interface ICastData {
+    adult: boolean;
+    gender: number | null;
+    id: number;
+    known_for_department: string;
+    name: string;
+    original_name: string;
+    popularity: number;
+    profile_path: string | null;
+    cast_id: number;
+    character: string;
+    credit_id: string;
+    order: number;
+}
+
+export interface ICrewData {
+    adult: boolean;
+    gender: number | null;
+    id: number;
+    known_for_department: string;
+    name: string;
+    original_name: string;
+    popularity: number;
+    profile_path: string | null;
+    cast_id: number;
+    department: string;
+    job: string;
+}
+
+interface ICreditsData {
+    cast: ICastData[];
+    crew: ICreditsData[];
+}
+
+export interface IVideoData {
+    id: string;
+    iso_639_1: string;
+    iso_3166_1: string;
+    key: string;
+    name: string;
+    site: string;
+    size: number;
+    type: string;
+}
+
+interface IVideosData {
+    results: IVideoData[];
+}
+
+export interface IMovieDetailData{
+    adult: boolean;
+    backdrop_path: string | null;
+    belongs_to_collection: null | {[key:string]:any};
+    budget: number;
+    genres: IGenreData[];
+    homepage: string | null;
+    id: number;
+    imdb_id: string | null;
+    original_language: string;
+    original_title: string;
+    overview: string | null;
+    popularity: number;
+    poster_path: string| null;
+    production_companies: IProductionCompanyData[];
+    production_countries: IProductionCountryData[];
+    release_date: string;
+    revenue: number;
+    runtime: number | null;
+    spoken_languages: ISpokenLanguageData[];
+    status: string;
+    tagline: string | null;
+    title: string;
+    video: boolean;
+    vote_average: number;
+    vote_count: number;
+    credits: ICreditsData;
+    videos: IVideosData;
+}
+
