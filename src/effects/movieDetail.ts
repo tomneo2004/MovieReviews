92178f75a21a9c92ed6f8abfc1db60f2c6a98133
Nelson <tomneo2004@gmail.com>
2020-11-22 12:18:55 +0800

    add movie detail side effect

--- src/effects/movieDetail.ts
+++ src/effects/movieDetail.ts
@@ -1,1 +1,38 @@
 
+import useSWR from 'swr';
+import axios from 'axios';
+import {IMovieDetailData} from '../utils/apiModelTypes';
+
+const apiRoute = `${process.env.NEXT_PUBLIC_API_BASE_ROUTE}/api/detail/movies`;
+
+const fetcher = async (url:string)=>{
+
+    const resp = await axios.get(url);
+    const data: IMovieDetailData = resp.data;
+    return data;
+}
+
+export interface IPopularMovies{
+    data: IMovieDetailData | null;
+    error: any | null;
+}
+
+/**
+ * Side effect for fetching movie detail
+ */
+export function useMovieDetail(id:number):IPopularMovies{
+    const {data, error} = useSWR(()=>`${apiRoute}?id=${id}`, fetcher);
+
+    if(error){
+        console.log(error);
+        return {
+            data: null,
+            error,
+        }
+    }
+
+    return {
+        data,
+        error: null,
+    }
+}
--- src/pages/detail/index.tsx
+++ src/pages/detail/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {useRouter} from 'next/router';
+import {useMovieDetail} from '../../effects/movieDetail';
 import Navigation from '../../components/movieReview/navigation/navigation';
 import PageLayout from '../../layouts/pageLayout';
 import Box from '@material-ui/core/Box/Box';
@@ -7,6 +8,9 @@ import Box from '@material-ui/core/Box/Box';
 const DetailPage = () => {
     const router = useRouter();
     const {id} = router.query as {[key:string]:string};
+    const {data, error} = useMovieDetail(Number(id))
+
+    console.log(data, error);
 
     return (
         <PageLayout
