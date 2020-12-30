93448f16e349984a2d23276f8a1b356ac1f1b382
Nelson <tomneo2004@gmail.com>
2020-12-30 14:15:50 +0800

    get rid of unexpect token or error

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,17 @@ module.exports = {
     "/.next"
   ],
   setupFilesAfterEnv: [
-    "<rootDir>/.ci/setupTests.ts"
+    "<rootDir>/.ci/setupEnzyme.ts"
   ],
+  globals: {
+    // we must specify a custom tsconfig for tests because we need the typescript transform
+    // to transform jsx into js rather than leaving it jsx such as the next build requires.  you
+    // can see this setting in tsconfig.jest.json -> "jsx": "react"
+    "ts-jest": {
+      tsconfig: "tsconfig.jest.json",
+      diagnostics: {
+        warnOnly: true
+      }
+    }
+  }
 };
