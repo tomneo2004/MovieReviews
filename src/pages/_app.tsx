cef18881d595b52b76513ce9396ec4e954a4fb13
Nelson <tomneo2004@gmail.com>
2021-01-22 18:07:02 +0800

    page transition indicator

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,16 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import theme from '../themes/defaultTheme';
 import type { AppProps /*, AppContext */ } from 'next/app';
 import MainLayout from '../layouts/mainLayout';
+import NProgress from 'nprogress';
+import Router from 'next/router'
+
+NProgress.configure({showSpinner:false})
+Router.events.on('routeChangeStart', (url) => {
+    console.log(`Loading: ${url}`)
+    NProgress.start()
+  })
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
 
 export default function MovieReviewApp(props:AppProps) {
     const {Component, pageProps} = props;
@@ -22,6 +32,8 @@ export default function MovieReviewApp(props:AppProps) {
         <Head>
             <title>MovieReviews</title>
             <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+            {/* Import CSS for nprogress */}
+            <link rel="stylesheet" type="text/css" href="/nprogress.css" />
         </Head>
         <ThemeProvider theme={theme}>
             {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
