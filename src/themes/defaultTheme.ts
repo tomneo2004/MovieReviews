6801c1f97a567b17e2a785fac80d26663bdc38c0
Nelson <tomneo2004@gmail.com>
2021-01-28 18:05:13 +0800

    add responsive for font size

--- src/themes/defaultTheme.ts
+++ src/themes/defaultTheme.ts
@@ -1,4 +1,4 @@
-import { createMuiTheme } from '@material-ui/core/styles';
+import { createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles';
 import { red } from '@material-ui/core/colors';
 
 // Create a theme instance.
@@ -25,4 +25,4 @@ const theme = createMuiTheme({
   },
 });
 
-export default theme;
+export default responsiveFontSizes(theme);
